88c01dd57d65fc76857c3501c631f04f61fee950
Oleg Rybkin <@ya.ru>
2025-07-24 18:08:23 +0300

    Закончили разработку

--- App.cs
+++ App.cs
@@ -1,11 +1,7 @@
 ﻿using Autodesk.Revit.UI;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 
 namespace Integrator
@@ -19,7 +15,7 @@ namespace Integrator
 
         public Result OnStartup(UIControlledApplication application)
         {
-            string ribbonTabName = "A101 Mod";
+            string ribbonTabName = "A101 Mod КР";
             string ribbonPanelModName = "Армирование";
 
             application.CreateRibbonTab(ribbonTabName);
@@ -31,7 +27,7 @@ namespace Integrator
             //Каркасы продавливания в фундаментной плите
             string punchingRebarAsseblyName = Path.Combine(directoryName, "PunchingFoundRebarModule.dll");
 
-            PushButtonData punchingRebarButtonData = new PushButtonData("PunchingFoundRebar", "Каркасы в фундаменте", punchingRebarAsseblyName, "PunchingFoundRebarModule.Model.PunchingFoundRebar");
+            PushButtonData punchingRebarButtonData = new PushButtonData("PunchingFoundRebar", "Поперечное армирование", punchingRebarAsseblyName, "PunchingFoundRebarModule.Model.PunchingFoundRebar");
             PushButton punchingRebarButton = ribbonPanelMod.AddItem(punchingRebarButtonData) as PushButton;
 
             BitmapImage punchingRebarLogo = new BitmapImage(new Uri("pack://application:,,,/Integrator;component/Images/PunchingRebarPic.ico"));
